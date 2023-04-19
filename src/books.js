eae14d3314d17771a3e8664dff442b1e230dbb03
Arajdian Altaf <arajdianaltaf123@gmail.com>
2023-04-19 11:40:23 +0700

    Added jsdocs for Book class

--- src/books.js
+++ src/books.js
@@ -1,6 +1,21 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const { nanoid } = require('nanoid');
 
+/**
+ * @typedef {Object} Book
+ * @property {string} id          - Id of the book.
+ * @property {string} name        - Name of the book.
+ * @property {number} year        - Year the book published.
+ * @property {string} author      - Author of the book.
+ * @property {string} summary     - Summary of the book.
+ * @property {string} publisher   - Publisher of the book.
+ * @property {number} pageCount   - Page count of the book.
+ * @property {number} readPage    - Current reading page.
+ * @property {boolean} finished   - Finished reading status.
+ * @property {boolean} reading    - Currently reading status.
+ * @property {Date} insertedAt    - Date of the book were inserted.
+ * @property {Date} updatedAt     - Date of the book were updated.
+ */
 class Book {
   constructor(name, year, author, summary, publisher, pageCount, readPage, reading) {
     this.id = nanoid(16);
