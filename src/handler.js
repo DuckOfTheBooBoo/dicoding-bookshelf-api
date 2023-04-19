6ef54bf4874778786b509e937836fee2ae65a8dc
Arajdian Altaf <arajdianaltaf123@gmail.com>
2023-04-19 12:41:48 +0700

    Added if statement to check body validity

--- src/handler.js
+++ src/handler.js
@@ -1,22 +1,43 @@
 const { Book, books } = require('./books');
 
+// Menambahkan book baru berdasarkan body yang diberikan oleh request
 const addBookHandler = (request, h) => {
   const {
-    name,
-    year,
-    author,
-    summary,
-    publisher,
-    pageCount,
-    readPage,
-    reading,
+    name = undefined,
+    year = undefined,
+    author = undefined,
+    summary = undefined,
+    publisher = undefined,
+    pageCount = undefined,
+    readPage = 0,
+    reading = false,
   } = request.payload;
+
+  // Melempar error 400 jika nama buku tidak dilampirkan
+  if (!name) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. Mohon isi nama buku',
+    });
+    response.code(400);
+    return response;
+
+  }
+
+  // Melempar error 400 jika readPage lebih besar dari pageCount
+  if (readPage > pageCount) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. Mohon isi nama buku',
+    });
+    response.code(400);
+    return response;
+  }
 
   const newBook = new Book(name, year, author, summary, publisher, pageCount, readPage, reading);
   books.push(newBook);
 
   const isSuccess = books.filter((book) => book.name === name).length > 0;
-
   if (isSuccess) {
     const response = h.response({
       status: 'success',
@@ -31,7 +52,7 @@ const addBookHandler = (request, h) => {
 
   const response = h.response({
     status: 'fail',
-    message: 'Buku gagal ditambahkan',
+    message: 'Catatan gagal ditambahkan',
   });
   response.code(500);
   return response;
