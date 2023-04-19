9c6d05d1a80875f41173f3f28d57e79ef080394c
Arajdian Altaf <arajdianaltaf123@gmail.com>
2023-04-19 11:40:08 +0700

    Added addBookHandler and getAllBookHandler

---
+++ src/handler.js
@@ -0,0 +1,46 @@
+const { Book, books } = require('./books');
+
+const addBookHandler = (request, h) => {
+  const {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+  } = request.payload;
+
+  const newBook = new Book(name, year, author, summary, publisher, pageCount, readPage, reading);
+  books.push(newBook);
+
+  const isSuccess = books.filter((book) => book.name === name).length > 0;
+
+  if (isSuccess) {
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil ditambahkan',
+      data: {
+        bookId: newBook.id,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku gagal ditambahkan',
+  });
+  response.code(500);
+  return response;
+};
+
+const getAllBooksHandler = () => ({
+  status: 'success',
+  data: {
+    books,
+  },
+});
+module.exports = { addBookHandler, getAllBooksHandler };
